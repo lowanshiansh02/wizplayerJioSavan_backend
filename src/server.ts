0a4056ce0b1c1aadfa955397e9a2ab63ac114310
Ansh Lowanshi <lowanshiansh@gmail.com>
2026-08-22 18:09:27 +0530

    feat: configure JioSaavn API for Render deployment

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+import { serve } from '@hono/node-server'
 import { AlbumController, ArtistController, SearchController, SongController } from '#modules/index'
 import { PlaylistController } from '#modules/playlists/controllers'
 import { App } from './app'
@@ -9,5 +11,15 @@ const app = new App([
   new ArtistController(),
   new PlaylistController()
 ]).getApp()
+
+if (typeof Bun === 'undefined') {
+  const port = Number(process.env.PORT) || 3000
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${port}`)
+  serve({
+    fetch: app.fetch,
+    port
+  })
+}
 
 export default app
